a2cd29c6e81de285e74ad96cb8bfd706a8d68cef
llmobinll <mobin30bill@gmail.com>
2024-07-14 12:31:00 +0330

    finished part 2

---
+++ assest/Js/script.js
@@ -0,0 +1,29 @@
+const header = document.querySelector(".header");
+const navbutton = document.querySelectorAll(".nav-item");
+const scrollHeader = () => {
+  if (this.scrollY >= 100) header.classList.add("scroll-header");
+  else header.classList.remove("scroll-header");
+};
+window.addEventListener("scroll", scrollHeader);
+
+const changeline = (filter) => {
+  navbutton.forEach((button) => {
+    if (button.dataset.filter === filter) {
+      button.classList.add("underline");
+    } else {
+      button.classList.remove("underline");
+    }
+  });
+};
+const changeClass = (button) => {
+  const filter = button.target.dataset.filter;
+  changeline(filter);
+};
+navbutton.forEach((button) => {
+  button.addEventListener("click", changeClass);
+});
+const loadingHandler = () => {
+  const container = document.querySelector(".container");
+  container.classList.add("container-visible");
+};
+window.addEventListener("load", loadingHandler);
